113fa34d8e6ba681f229bf10274b5cd28844a4a7
nouka <t-nouka@oz-vision.co.jp>
2015-12-26 16:35:33 +0900

    7-1を追加

---
+++ examples/7-1.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Adapterパターンの実装
+ */
+
+class Shape {}
+
+class Circle extends Shape
+{
+    private $myXXCircle;
+
+    public function __construct()
+    {
+        $this->myXXCircle = new XXCircle();
+    }
+
+    public function display()
+    {
+        $this->myXXCircle->displayIt();
+    }
+}
