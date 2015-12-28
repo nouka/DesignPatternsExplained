5dca24995a765d4339e7e0fb2d2b725d1e67251a
nouka <t-nouka@oz-vision.co.jp>
2015-12-28 15:22:30 +0900

    21-1を追加

---
+++ examples/21-1.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Singletonパターン.
+ */
+class USTax extends CalcTax
+{
+    private static $instance;
+
+    private function __construct()
+    {
+    }
+
+    public static function getInstance()
+    {
+        if (!self::$instance) {
+            self::$instance = new self();
+        }
+
+        return self::$instance;
+    }
+}
